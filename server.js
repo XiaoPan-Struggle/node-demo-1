5a626b0a9f00f4fde09f45862a033e6d8482c10f
XiaoPan <2489964425@qq.com>
2020-12-12 09:59:08 +0800

    404

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ var server = http.createServer(function(request, response){
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
+    response.write(`访问的内容不存在`)
     response.end()
   }
 
